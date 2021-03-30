d6669efabc20a1e632f970c7e5945fffac737da5
Xavier Solau <xaviersolau@yahoo.fr>
2021-03-31 01:04:36 +0200

    Add basic tests.

---
+++ src/tests/SoloX.BlazorLayout.UTest/Containers/InlineContainerTest.cs
@@ -0,0 +1,29 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="InlineContainerTest.cs" company="Xavier Solau">
+// Copyright © 2021 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.BlazorLayout.Containers;
+using SoloX.BlazorLayout.UTest.Core;
+using Xunit;
+
+namespace SoloX.BlazorLayout.UTest.Containers
+{
+    public class InlineContainerTest
+    {
+        [Fact]
+        public void ItShouldRenderWithTheGivenId()
+        {
+            PanelHelpers.AssertIdIsProperlyRendered<InlineContainer>();
+        }
+
+        [Fact]
+        public void ItShouldRenderWithTheGivenClass()
+        {
+            PanelHelpers.AssertClassIsProperlyRendered<InlineContainer>();
+        }
+    }
+}
---
+++ src/tests/SoloX.BlazorLayout.UTest/Containers/ResizeContainerTest.cs
@@ -0,0 +1,29 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="ResizeContainerTest.cs" company="Xavier Solau">
+// Copyright © 2021 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.BlazorLayout.Containers;
+using SoloX.BlazorLayout.UTest.Core;
+using Xunit;
+
+namespace SoloX.BlazorLayout.UTest.Containers
+{
+    public class ResizeContainerTest
+    {
+        [Fact]
+        public void ItShouldRenderWithTheGivenId()
+        {
+            PanelHelpers.AssertIdIsProperlyRendered<ResizeContainer>();
+        }
+
+        [Fact]
+        public void ItShouldRenderWithTheGivenClass()
+        {
+            PanelHelpers.AssertClassIsProperlyRendered<ResizeContainer>();
+        }
+    }
+}
