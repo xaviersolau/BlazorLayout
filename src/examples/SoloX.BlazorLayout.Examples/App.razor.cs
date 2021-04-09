096665ec6e480b59cb8cddce114b8c85ba62591d
Xavier Solau <xaviersolau@yahoo.fr>
2021-04-09 02:12:56 +0200

    Update example.

---
+++ src/examples/SoloX.BlazorLayout.Examples/App.razor.cs
@@ -0,0 +1,17 @@
+﻿using MatBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoloX.BlazorLayout.Examples
+{
+    public partial class App
+    {
+        private MatTheme Theme { get; } = new MatTheme()
+        {
+            //Secondary = MatThemeColors.Red._600.Value,
+            Surface = MatThemeColors.LightBlue._100.Value,
+        };
+    }
+}
---
+++ src/examples/SoloX.BlazorLayout.Examples/Shared/ThemeEx.cs
@@ -0,0 +1,16 @@
+﻿using MatBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoloX.BlazorLayout.Examples.Shared
+{
+    public static class ThemeEx
+    {
+        public static string GetClass(this MatTheme theme, string classToAdd)
+        {
+            return $"{theme.GetClass()} {classToAdd}";
+        }
+    }
+}
