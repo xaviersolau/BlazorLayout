631c2f51f774daa2dc7a83d8373e1ee0f234e6ab
Xavier Solau <xaviersolau@yahoo.fr>
2021-05-05 22:14:50 +0200

    Play with colors in samples.

--- src/examples/SoloX.BlazorLayout.Examples.WithBlazorise/App.razor.cs
+++ src/examples/SoloX.BlazorLayout.Examples.WithBlazorise/App.razor.cs
@@ -10,6 +10,28 @@ namespace SoloX.BlazorLayout.Examples.WithBlazorise
     {
         private Theme Theme { get; } = new Theme()
         {
+            BackgroundOptions = new ThemeBackgroundOptions()
+            {
+                Primary = ThemeColors.Purple._700.Value,
+                Secondary = ThemeColors.LightGreen._700.Value,
+                Dark = ThemeColors.Gray._700.Value,
+                Light = ThemeColors.LightBlue._200.Value,
+                Body = ThemeColors.Lime._100.Value,
+            },
+            ColorOptions = new ThemeColorOptions()
+            {
+                Primary = ThemeColors.Purple._700.Value,
+                Secondary = ThemeColors.LightGreen._700.Value,
+                Dark = ThemeColors.Gray._700.Value,
+                Light = ThemeColors.LightBlue._200.Value,
+            },
+            TextColorOptions = new ThemeTextColorOptions()
+            {
+                Primary = ThemeColors.Purple._700.Value,
+                Secondary = ThemeColors.LightGreen._700.Value,
+                Dark = ThemeColors.Gray._700.Value,
+                Light = ThemeColors.LightBlue._200.Value,
+            },
         };
     }
 }
