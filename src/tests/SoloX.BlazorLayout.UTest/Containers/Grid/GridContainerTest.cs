bebf2acb76565c0896276369dcf5b5cfae8dd697
Xavier Solau <xaviersolau@yahoo.fr>
2023-10-25 19:49:34 +0200

    Add some tests on Grid Cell Location.

--- src/tests/SoloX.BlazorLayout.UTest/Containers/Grid/GridContainerTest.cs
+++ src/tests/SoloX.BlazorLayout.UTest/Containers/Grid/GridContainerTest.cs
@@ -130,5 +130,132 @@ namespace SoloX.BlazorLayout.UTest.Containers.Grid
             style2.Should().ContainSingle(x => x.Name == "grid-column" && x.Value == "2");
             style2.Should().ContainSingle(x => x.Name == "grid-row" && x.Value == "2");
         }
+
+        [Theory]
+        [InlineData("left", "top", "1", "1")]
+        [InlineData("right", "top", "2", "1")]
+        [InlineData("left", "bottom", "1", "2")]
+        [InlineData("right", "bottom", "2", "2")]
+        [InlineData("0", "0", "1", "1")]
+        [InlineData("1", "0", "2", "1")]
+        [InlineData("0", "1", "1", "2")]
+        [InlineData("1", "1", "2", "2")]
+        public void ItShouldRenderCellsOnTheRightNamedLocation(string colName, string rowName, string expectedCol, string expectedRow)
+        {
+            var cellId1 = "cell-id1";
+
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<GridContainer>(
+                builder =>
+                {
+                    builder.AddChildContent<GridColumn>(
+                        colBuilder =>
+                        {
+                            colBuilder.Add(c => c.Name, "left");
+                        });
+                    builder.AddChildContent<GridColumn>(
+                        colBuilder =>
+                        {
+                            colBuilder.Add(c => c.Name, "right");
+                        });
+                    builder.AddChildContent<GridRow>(
+                        rowBuilder =>
+                        {
+                            rowBuilder.Add(c => c.Name, "top");
+                        });
+
+                    builder.AddChildContent<GridRow>(
+                        rowBuilder =>
+                        {
+                            rowBuilder.Add(c => c.Name, "bottom");
+                        });
+                    builder.AddChildContent<GridCell>(
+                        cellBuilder =>
+                        {
+                            cellBuilder.Add(c => c.Column, colName);
+                            cellBuilder.Add(c => c.Row, rowName);
+                            cellBuilder.Add(c => c.Id, cellId1);
+                        });
+                });
+
+            // Assert
+            cut.Nodes.Length.Should().Be(1);
+
+            var cellElt1 = cut.Find($"#{cellId1}");
+
+            var style1 = StyleHelper.LoadStyleAttribute(cellElt1);
+            style1.Should().ContainSingle(x => x.Name == "grid-column" && x.Value == expectedCol);
+            style1.Should().ContainSingle(x => x.Name == "grid-row" && x.Value == expectedRow);
+        }
+
+        [Theory]
+        [InlineData("left", "left", "top", "top", "1/2", "1/2")]
+        [InlineData("left", "middle", "top", "bottom", "1/3", "1/4")]
+        [InlineData("middle", "right", "middle", "bottom", "2/4", "2/4")]
+        [InlineData("left", "right", "bottom", "bottom", "1/4", "3/4")]
+        public void ItShouldRenderCellsOnTheRightNamedLocationWithSpan(string colNameStart, string colNameEnd, string rowNameStart, string rowNameEnd, string expectedCol, string expectedRow)
+        {
+            var cellId1 = "cell-id1";
+
+            // Arrange
+            using var ctx = new TestContext();
+
+            // Act
+            var cut = ctx.RenderComponent<GridContainer>(
+                builder =>
+                {
+                    builder.AddChildContent<GridColumn>(
+                        colBuilder =>
+                        {
+                            colBuilder.Add(c => c.Name, "left");
+                        });
+                    builder.AddChildContent<GridColumn>(
+                        colBuilder =>
+                        {
+                            colBuilder.Add(c => c.Name, "middle");
+                        });
+                    builder.AddChildContent<GridColumn>(
+                        colBuilder =>
+                        {
+                            colBuilder.Add(c => c.Name, "right");
+                        });
+                    builder.AddChildContent<GridRow>(
+                        rowBuilder =>
+                        {
+                            rowBuilder.Add(c => c.Name, "top");
+                        });
+                    builder.AddChildContent<GridRow>(
+                        rowBuilder =>
+                        {
+                            rowBuilder.Add(c => c.Name, "middle");
+                        });
+                    builder.AddChildContent<GridRow>(
+                        rowBuilder =>
+                        {
+                            rowBuilder.Add(c => c.Name, "bottom");
+                        });
+                    builder.AddChildContent<GridCell>(
+                        cellBuilder =>
+                        {
+                            cellBuilder.Add(c => c.Column, colNameStart);
+                            cellBuilder.Add(c => c.ColumnEnd, colNameEnd);
+                            cellBuilder.Add(c => c.Row, rowNameStart);
+                            cellBuilder.Add(c => c.RowEnd, rowNameEnd);
+                            cellBuilder.Add(c => c.Id, cellId1);
+                        });
+                });
+
+            // Assert
+            cut.Nodes.Length.Should().Be(1);
+
+            var cellElt1 = cut.Find($"#{cellId1}");
+
+            var style1 = StyleHelper.LoadStyleAttribute(cellElt1);
+            style1.Should().ContainSingle(x => x.Name == "grid-column" && x.Value == expectedCol);
+            style1.Should().ContainSingle(x => x.Name == "grid-row" && x.Value == expectedRow);
+        }
     }
 }
