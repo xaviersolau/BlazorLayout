b00ae28abd585517c3bc72b4b44a4cbdc863dd7d
Xavier Solau <xaviersolau@yahoo.fr>
2024-10-22 22:57:02 +0200

    Fix screenshot tests

--- src/tests/SoloX.BlazorLayout.E2ETests/Examples/AppBlazoriseTests.cs
+++ src/tests/SoloX.BlazorLayout.E2ETests/Examples/AppBlazoriseTests.cs
@@ -16,8 +16,11 @@ namespace SoloX.BlazorLayout.E2ETests.Examples
 {
     public class AppBlazoriseTests
     {
-        [Fact]
-        public async Task IsShouldDisplayComponentExample()
+        [Theory]
+        [InlineData(Browser.Chromium)]
+        [InlineData(Browser.Firefox)]
+        [InlineData(Browser.Webkit)]
+        public async Task IsShouldDisplayComponentExample(Browser browser)
         {
             var builder = PlaywrightTestBuilder.Create()
                 .WithLocalHost(configuration =>
@@ -46,11 +49,11 @@ namespace SoloX.BlazorLayout.E2ETests.Examples
                 .WithPlaywrightNewContextOptions(opt =>
                 {
                     // Set up the browser view port size.
-                    opt.ViewportSize = new ViewportSize() { Width = 800, Height = 600 };
+                    opt.ViewportSize = new ViewportSize() { Width = 1000, Height = 600 };
                 });
 
             var playwrightTest = await builder
-                .BuildAsync(Browser.Chromium)
+                .BuildAsync(browser)
                 .ConfigureAwait(true);
 
             await using var _ = playwrightTest.ConfigureAwait(false);
@@ -65,8 +68,8 @@ namespace SoloX.BlazorLayout.E2ETests.Examples
 
             pngBuffer.Should().NotBeNull();
 
-            var isDifferences = ImageComparison.CompareAndGenerateDifferenceImage(
-                $"../../../Examples/Screenshots/AppBlazoriseTests.IsShouldDisplayComponentExample{Browser.Chromium}.png",
+            var isDifferences = ImageComparison.CompareAndGenerateImageDifference(
+                $"../../../Examples/Screenshots/AppBlazoriseTests.IsShouldDisplayComponentExample{browser}.png",
                 pngBuffer!);
 
             isDifferences.Should().BeFalse();
